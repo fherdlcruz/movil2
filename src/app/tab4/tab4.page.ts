17e9b78e2038a1792f6b720c9abea642eb0558dd
Fernando Hernandez <fherdlcruz@gmail.com>
2021-01-13 19:25:28 -0600

    Tema 1. Modulos de apps Tema 2. Modulo de Rutas Tema 3. Modulo de Formularios

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+})
+export class Tab4Page implements OnInit {
+
+  public nombre:string = "";
+  private paterno:string="";
+  materno:any="";
+  edad:number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("Iniciando el TAB 4 PAGE");
+  }
+
+  aumentarAnio():void{
+    this.edad +=1;
+  }
+  restarAnio():void{
+    this.edad -=1;
+  }
+
+}
